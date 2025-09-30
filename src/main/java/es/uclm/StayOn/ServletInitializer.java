bc4cbf49ea1888263d18ade9d65b66771c684627
zzzaaii <zaida.burgos@alu.uclm.es>
2025-09-30 17:06:08 +0200

    Comprobado

--- src/main/java/es/uclm/StayOn/ServletInitializer.java
+++ src/main/java/es/uclm/StayOn/ServletInitializer.java
@@ -9,7 +9,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(StayOnApplication.class);
 		
-		//compruebo que funciona x2
 	}
 
 }
