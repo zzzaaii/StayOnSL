981c33d147c758a9b03cc8cc77bf1de549194589
Jespul10 <jesus.pulido@alu.uclm.es>
2025-10-13 09:50:51 +0200

    practica 3

--- src/main/java/es/uclm/StayOn/StayOnApplication.java
+++ src/main/java/es/uclm/StayOn/StayOnApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("es.uclm.library.persistence") // <--- fuerza escaneo de repositorios
+@EnableJpaRepositories("es.uclm.StayOn.persistence") // <--- fuerza escaneo de repositorios
 public class StayOnApplication {
     public static void main(String[] args) {
         SpringApplication.run(StayOnApplication.class, args);
