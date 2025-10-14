e90904c114e161910e1d4680f147a13809aaddde
zzzaaii <zaida.burgos@alu.uclm.es>
2025-10-14 15:19:47 +0200

    Se me habia olvidado la clase de usuario jeje

--- src/main/java/es/uclm/StayOn/entity/Usuario.java
+++ src/main/java/es/uclm/StayOn/entity/Usuario.java
@@ -1,5 +1,10 @@
 package es.uclm.StayOn.entity;
 
 public class Usuario {
-
+	private String login;
+	private String pass;
+	private String nombre;
+	private String apellidos;
+	private String dirección;
+	private int attribute;
 }
